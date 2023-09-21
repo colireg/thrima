4040f7f5558bb0cb8f37de8eeb9f61b4d54d06c1
matty <mattykhaliliraad@gmail.com>
2023-09-21 19:34:29 +0330

    Added filters to get all books endpoint

--- src/Thrima.Application/Books/Queries/GetAllBooksQuery.cs
+++ src/Thrima.Application/Books/Queries/GetAllBooksQuery.cs
@@ -4,10 +4,26 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Thrima.Application.Services;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using System.Data;
 
 namespace Thrima.Application.Books.Queries;
 
-public record GetAllBooksQuery() : IRequest<IEnumerable<GetAllBooksQueryListItem>>;
+public record GetAllBooksQuery(
+    string? Title,
+    string? Author) : IRequest<IEnumerable<GetAllBooksQueryListItem>>;
+
+public class GetAllBooksQueryValidator : AbstractValidator<GetAllBooksQuery>
+{
+    public GetAllBooksQueryValidator()
+    {
+        RuleFor(x => x.Title)
+            .MaximumLength(256);
+        
+        RuleFor(x => x.Author)
+            .MaximumLength(256);
+    }
+}
 
 public record GetAllBooksQueryListItem(
     Guid Id,
@@ -27,18 +43,34 @@ public class GetAllBooksQueryHandler
 {
     private readonly IMapper _mapper;
     private readonly IAppDbContext _appDbContext;
+    private readonly IValidator<GetAllBooksQuery> _validator;
 
-    public GetAllBooksQueryHandler(IMapper mapper, IAppDbContext appDbContext)
+    public GetAllBooksQueryHandler(
+        IMapper mapper,
+        IAppDbContext appDbContext,
+        IValidator<GetAllBooksQuery> validator)
     {
         _mapper = mapper;
         _appDbContext = appDbContext;
+        _validator = validator;
     }
 
     public async Task<IEnumerable<GetAllBooksQueryListItem>> Handle(GetAllBooksQuery request, CancellationToken cancellationToken)
     {
-        var result = await _appDbContext
+        await _validator.ValidateAndThrowAsync(request, cancellationToken);
+
+        var query = _appDbContext
             .Books
             .AsNoTracking()
+            .AsQueryable();
+        
+        if (request.Title is not null)
+            query = query.Where(x => x.Title.StartsWith(request.Title));
+
+        if (request.Author is not null)
+            query = query.Where(x => x.Author.StartsWith(request.Author));
+
+        var result = await query
             .ProjectTo<GetAllBooksQueryListItem>(_mapper.ConfigurationProvider)
             .ToListAsync(cancellationToken);
 
